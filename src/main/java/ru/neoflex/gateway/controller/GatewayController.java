6a7b96c81373e51c2a2303b94da2e838c6cb200e
huawei <vo.gr2002@gmail.com>
2023-02-10 15:34:59 +0300

    add log

--- src/main/java/ru/neoflex/gateway/controller/GatewayController.java
+++ src/main/java/ru/neoflex/gateway/controller/GatewayController.java
@@ -3,6 +3,7 @@ package ru.neoflex.gateway.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import ru.neoflex.gateway.service.FeignDealClient;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Шлюз",
@@ -27,37 +29,51 @@ public class GatewayController {
     @Operation(summary = "Получение предложений по кредиту")
     @PostMapping(value = "/application")
     public List<LoanOfferDTO> getLoanOffers(@RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
-        return feignApplicationClient.getLoanOffers(loanApplicationRequestDTO);
+        log.info("Получение предложений, loanApplicationRequestDTO {}", loanApplicationRequestDTO);
+        List<LoanOfferDTO> loanOffers = feignApplicationClient.getLoanOffers(loanApplicationRequestDTO);
+        log.info("Полученные предложения {}", loanOffers);
+        return loanOffers;
     }
 
     @Operation(summary = "Установка принятого предложения")
     @PostMapping(value = "/application/apply")
     public void applyOffer(@RequestBody LoanOfferDTO loanOfferDTO) {
+        log.info("Принятие предложения, loanOfferDTO {}", loanOfferDTO);
         feignApplicationClient.applyOffer(loanOfferDTO);
+        log.info("Предложение принято, loanOfferDTO {}", loanOfferDTO);
     }
 
     @Operation(summary = "Окончательная регистрация")
     @PostMapping(value = "application/registration/{applicationId}")
     public void calculateRequest(@RequestBody FinishRegistrationRequestDTO finishRegistrationRequestDTO,
                                  @PathVariable Long applicationId) {
+        log.info("Окончательная регистрация, finishRegistrationRequestDTO {}, applicationId {}",
+                finishRegistrationRequestDTO, applicationId);
         feignDealClient.calculateRequest(finishRegistrationRequestDTO, applicationId);
+        log.info("Регистрация окончена");
     }
 
     @Operation(summary = "Получение документов по кредиту на почту")
     @PostMapping(value = "document/{applicationId}")
     public void sendRequestForDocument(@PathVariable Long applicationId) {
+        log.info("Получение документов на почту, applicationId {}", applicationId);
         feignDealClient.sendRequestForDocument(applicationId);
+        log.info("Документы получены");
     }
 
     @Operation(summary = "Запрос на подписание документов и получение кода на почту")
     @PostMapping(value = "document/{applicationId}/sign")
     public void sendRequestForSignDocument(@PathVariable Long applicationId) {
+        log.info("Получение кода на почту, applicationId {}", applicationId);
         feignDealClient.sendRequestForSignDocument(applicationId);
+        log.info("Код получен");
     }
 
     @Operation(summary = "Подпись документов")
     @PostMapping(value = "document/{applicationId}/{code}")
     public void signDocument(@PathVariable Long applicationId, @PathVariable Integer code) {
+        log.info("Подписание документов, applicationId {} code {}", applicationId, code);
         feignDealClient.signDocument(applicationId, code);
+        log.info("Документы подписаны");
     }
 }
